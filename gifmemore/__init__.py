990cf53f36ede1ee387017d05f8c38c299e2cc0e
Shanwis <ashsanthosh8@gmail.com>
2026-05-26 19:57:09 +0530

    Bump version to 1.0.1

--- gifmemore/__init__.py
+++ gifmemore/__init__.py
@@ -4,5 +4,5 @@ from .config import GIFConfig
 from .filters import FilterBuilder
 from .creator import GIFCreator
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __all__ = ["GIFConfig", "FilterBuilder", "GIFCreator"]
